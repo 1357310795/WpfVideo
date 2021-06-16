a9edb2083110df1c01c00f699fc051278e49b468
Hikari <1357310795@qq.com>
2021-06-16 14:59:20 +0800

    222

--- WpfApp1/MainWindow.xaml.vb
+++ WpfApp1/MainWindow.xaml.vb
@@ -1,36 +1,11 @@
-﻿
-Imports System.Runtime.InteropServices
-Imports System.Windows.Interop
-Imports AudioSwitcher.AudioApi.CoreAudio
+﻿Imports System.Runtime.InteropServices
 
-Public Module rrrr
-    Public Sub Main1()
-        VideoPlayerController.AudioManager.SetMasterVolume(40)
-        VideoPlayerController.AudioManager.SetMasterVolumeMute(False)
-        Dim m As New MainWindow2
-        Application.Current.MainWindow = m
-        m.WindowStyle = WindowStyle.None
-        m.Width = 0
-        m.Height = 0
-        m.mediaUriElement.LoadedBehavior = MediaState.Manual
-        m.mediaUriElement.Volume = 60
-        m.Top = 0
-        m.Left = 0
-        m.Opacity = 0
-        m.Show()
-        m.startplay()
-        System.Threading.Thread.Sleep(100)
-        Dim workw = GetWorkerW()
-        Console.WriteLine(workw)
-        SetParent((New WindowInteropHelper(m)).Handle, workw)
-        MoveWindow((New WindowInteropHelper(m)).Handle, 0, 0, 1920, 1080, True)
-
-        m.Opacity = 1
+Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        GetWorkerW()
     End Sub
-
-
     <DllImport("user32.dll")>
-    Public Function SendMessageTimeout(ByVal hWnd As IntPtr,
+    Public Shared Function SendMessageTimeout(ByVal hWnd As IntPtr,
                                               ByVal wMsg As Integer,
                                               ByVal wParam As Integer,
                                               ByVal lParam As Integer,
@@ -43,30 +18,30 @@ Public Module rrrr
     Public Delegate Function EnumWindowsProc(ByVal hwnd As IntPtr, ByVal lParam As IntPtr) As Boolean
 
     <DllImport("user32")>
-    Public Function EnumWindows(ByVal lpEnumFunc As EnumWindowsProc, ByVal lParam As IntPtr) As Boolean
+    Public Shared Function EnumWindows(ByVal lpEnumFunc As EnumWindowsProc, ByVal lParam As IntPtr) As Boolean
 
     End Function
 
     <DllImport("user32")>
-    Public Function FindWindowEx(ByVal hWndParent As IntPtr, ByVal hWndChildAfter As IntPtr, ByVal lpWindowClass As String, ByVal lpWindowName As String) As IntPtr
+    Public Shared Function FindWindowEx(ByVal hWndParent As IntPtr, ByVal hWndChildAfter As IntPtr, ByVal lpWindowClass As String, ByVal lpWindowName As String) As IntPtr
 
     End Function
 
 
     <DllImport("user32")>
-    Public Function SetParent(ByVal hWndChild As IntPtr, ByVal hWndNewParent As IntPtr) As IntPtr
+    Public Shared Function SetParent(ByVal hWndChild As IntPtr, ByVal hWndNewParent As IntPtr) As IntPtr
 
     End Function
 
     <DllImport("user32.dll")>
-    Public Function MoveWindow(ByVal handle As IntPtr, ByVal x As Integer, ByVal y As Integer, ByVal nWidth As Integer, ByVal nHeight As Integer, ByVal bRepaint As Boolean) As Boolean
+    Public Shared Function MoveWindow(ByVal handle As IntPtr, ByVal x As Integer, ByVal y As Integer, ByVal nWidth As Integer, ByVal nHeight As Integer, ByVal bRepaint As Boolean) As Boolean
 
     End Function
-    Public Function GetWorkerW() As IntPtr
+    Public Shared Function GetWorkerW() As IntPtr
         Dim progmanHandle = FindWindowEx(IntPtr.Zero, IntPtr.Zero, "Progman", Nothing)
         Dim result = Nothing
         SendMessageTimeout(progmanHandle, &H52C, 0, 0, 0, 1000, result)
-        System.Threading.Thread.Sleep(100)
+        Threading.Thread.Sleep(100)
         Dim workerWHandle = IntPtr.Zero
         EnumWindows(New EnumWindowsProc(Function(topHandle, topParamHandle)
                                             Dim shellHandle = FindWindowEx(topHandle, IntPtr.Zero, "SHELLDLL_DefView", Nothing)
@@ -79,4 +54,5 @@ Public Module rrrr
                                         End Function), IntPtr.Zero)
         Return workerWHandle
     End Function
-End Module
+
+End Class
